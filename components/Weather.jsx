d3defc5631401976d12485a3e7b1164b23f16d0a
rebeccatuffnell <rctuffnell@gmail.com>
2022-08-06 22:38:16 +0100

    successfully fetches data, now working on fahrenheit conversion

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,26 +1,27 @@
+import Image from "next/image";
 import React from "react";
-import Home from "../../pages";
 
 const Weather = ({ data }) => {
+  console.log(data);
   return (
-    <div className="bg-black/50 relative p-8 rounded-md">
-    <div className="relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10">
-      {/* top */}
+    <div className="bg-black/50 rounded-md relative flex flex-col justify-between max-w-[500px] w-full m-auto p-4 text-gray-300 z-10">
+      {/* Top */}
+      <p className="text-3xl text-center pb-6">Weather in {data.name}</p>
       <div className="relative flex justify-between pt-12">
         <div className="flex flex-col items-center">
           <Image
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-            alt="weather icon"
+            alt="/"
             width="100"
             height="100"
           />
           <p className="text-2xl">{data.weather[0].main}</p>
         </div>
-        <p className="text-9xl">{data.main.temp.toFixed(0)}&#8451;</p>
+        <div>
+          <p className="text-6xl">{data.main.temp.toFixed(0)}&#8451;</p>
+          <p className="text-6xl">{data.main.temp.toFixed(0)}&#8457;</p>
+        </div>
       </div>
-      {/* bottom */}
-      
-    </div>
     </div>
   );
 };
