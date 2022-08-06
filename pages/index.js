089d17e629a32bd38b825b5d78d160fca8e60f09
rebeccatuffnell <rctuffnell@gmail.com>
2022-08-06 22:41:19 +0100

    background updated

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
         {/* Background image */}
         <Image
-          // src='https://images.unsplash.com/photo-1542272201-b1ca555f8505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+          src='https://images.unsplash.com/photo-1558486012-817176f84c6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1804&q=80'
           layout='fill'
           className='object-cover'
         />
