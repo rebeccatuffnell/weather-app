94b3bcf4e3b988ebf8df29a22c2076b925bebf3f
rebeccatuffnell <rctuffnell@gmail.com>
2022-08-13 17:44:57 +0100

    can now generate correct date and time of search city

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -2,40 +2,32 @@ import Image from "next/image";
 import React from "react";
 
 const Weather = ({ data }) => {
+  // console.table(data);
   console.log(data);
 
-  // create Date object for current location
-  let d = new Date();
-  console.log(d);
-
-  // convert to msec since Jan 1 1970
-  let localTime = d.getTime();
-  console.log(localTime);
-
-  // obtain local UTC offset and convert to msec
-  let localOffset = d.getTimezoneOffset() * 60000;
-  console.log(localOffset);
-
-  // obtain UTC time in msec
-  let utc = localTime + localOffset;
-  console.log(utc);
-
-  // obtain and add destination’s UTC time offset
-  // for example, Bombay
-  // which is UTC + 5.5 hours
-  let offset = 5.5;
-  let bombay = utc + 3600000 * offset;
-  console.log(bombay);
-
-  // convert msec value to date string
-  let nd = new Date(bombay);
+  let d = new Date()
+  console.log(d)
+  let localTime = d.getTime()
+  console.log(localTime)
+  let localOffset = d.getTimezoneOffset() * 60000
+  console.log(localOffset)
+  let utc = localTime + localOffset
+  console.log(utc)
+  let dateAndTime = utc + (1000 * data.timezone)
+  console.log(dateAndTime)
+  const nd = new Date(dateAndTime)
+  console.log(nd)
+  // let dt = nd.slice(0, 24);
+  // console.log(nd.substring(0, 24))
+  
 
   return (
     <div className="relative flex justify-between pt-12">
       <div className="bg-black/50 rounded-md relative flex flex-col justify-between max-w-[480px] w-full m-auto p-8 text-gray-300 z-10">
         {/* Top */}
         <p className="text-3xl text-center pb-6">{data.name}</p>
-        <p className="text-2xl text-center pb-6">{nd.toLocaleString()}</p>
+        <p className="text-3xl text-center pb-6">{data.sys.country}</p>
+        <p className="text-2xl text-center pb-6">{nd}</p>
         <div className="flex flex-col items-center m-8">
           <Image
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
